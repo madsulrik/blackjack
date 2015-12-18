ff88362d1cc5b6351e8d57a6d20bcf0f03d3d406
KvelaGorrrrnio <Anders@voreswww.dk>
2015-12-18 12:00:46 +0100

    Klasser
    Er næsten færdig med funktionerne til klasserne. Mangler lige at fikse
    Player.score og Hand.shuffle

--- src/blackjack.fsx
+++ src/blackjack.fsx
@@ -1,7 +1,7 @@
 ///
 ///
 ///
-type Suits = Spade | Heart | Diamond | Club
+type Suits = Spades | Hearts | Diamonds | Clubs
 
 ///
 ///
@@ -12,10 +12,10 @@ type Card(value, suit) =
   member this.toString() =
     let suit =
       match this.suit with
-      | Spade -> "♠"
-      | Heart -> "♥"
-      | Diamond -> "♦"
-      | Club -> "♣"
+      | Spades -> "♠"
+      | Hearts -> "♥"
+      | Diamonds -> "♦"
+      | Clubs -> "♣"
     let value =
       match this.value with
       | 1 ->  "A"
@@ -29,29 +29,61 @@ type Card(value, suit) =
 ///
 ///
 type Hand() = 
-  member this.cards:(Card array) = [||]
-  member this.drop() = this.cards
-  member this.draw card = Array.append this.cards [|card|]
-  member this.shuffle() = this.cards
+  let mutable hand:(Card array) = [||]
+  member this.cards with get () = hand
+  member this.drop() =
+    let lastIndex = (Array.length hand)-1 
+    let card = hand.[0]
+    hand <- hand.[1..lastIndex]
+    printfn "%A" (card.toString())
+    card    
+  member this.draw (card:Card) = hand <- Array.append [|card|] hand 
+  member this.shuffle() = 
+    let len = Array.length hand
+    let testCard = new Card(-1,Spades)
+    let newHand = Array.create len testCard
+    let test (card:Card) = card.toString()=testCard.toString()
+    let rnd = System.Random()
+    for i in 0..(len-1) do
+      let mutable j = rnd.Next(0,len)
+      while test hand.[j] do
+        j <- rnd.Next(0,len)
+      newHand.[i] <- hand.[j]
+      hand.[j] <- testCard
+    hand <- [||]//newHand
 
 ///
 ///
 ///
 type Player(name, index, AI) =
+  let h = new Hand()
   member this.name:string = name
   member this.index:int = index
-  member this.hand:Hand = new Hand()
+  member this.hand with get() = h
   member this.AI:bool = AI
-  member this.isBusted() = false
+  member this.score =
+    //implement 1 or 11 with A
+    let mutable score = 0
+    for card in this.hand.cards do
+      score <- score + card.value
+    score
+  member this.isBusted() = (this.score>=21)
+  new(name, index) =
+    Player(name, index, false)
+
+let printHand (hand:Hand) =
+  let len = Array.length hand.cards
+  for card in hand.cards do
+    printf "%s " (card.toString())
+  if len = 0 then printf "No cards!"
+  printfn ""
 
 ///
 ///
 ///
 type Game(dealer, players, numberOfPlayers) =
+  let s = new Hand()
   member this.dealer = dealer
   member this.players = players
   member this.numberOfPlayers = numberOfPlayers
-  member this.stack = new Hand()
-  
-let card = new Card(1, Heart)
-printfn "%s" (card.toString())
+  member this.stack = s
