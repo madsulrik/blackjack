3b03d5fe4381102735aa81034d1a0e96bb87c79a
KvelaGorrrrnio <Anders@voreswww.dk>
2015-12-22 14:21:50 +0100

    Klasser er dokumenteret

--- src/blackjack.fsx
+++ src/blackjack.fsx
@@ -1,12 +1,11 @@
-///
-///
-///
+/// <summary>Enumeration representing card-suits</summary>
 type Suits = Spades | Hearts | Diamonds | Clubs
 
-///
-///
-///
-type Card(value, suit) =
+/// <summary>Card is an object representing af card, with value and suit</summary>
+/// <param name="value">Integer representing the card value. 1 is A, 11-13 are 
+/// picturecards</param>
+/// <param name="suit">Suits enumeration representing the suit of the card</param>
+type Card(value,suit) =
   member this.value:int = value
   member this.suit:Suits = suit
   member this.toString() =
@@ -25,9 +24,10 @@ type Card(value, suit) =
       | x -> sprintf "%d" x
     sprintf "%s%s" suit value
 
-///
-///
-///
+/// <summary>Hand is an object representing a players hand. A Hand-object can 
+/// draw (Hand.draw card) or drop (Hand.drop) a card. The hand can be shuffled 
+/// (Hand.shuffle) and replaced by a new Card Array (Hand.replace cards) </summary>
+/// <param name="hand">Card Array that represents the cards on the hand (optional)</param>
 type Hand(hand) = 
   let mutable c:(Card array) = hand
   member this.cards with get() = c
@@ -53,19 +53,26 @@ type Hand(hand) =
     c <- newHand
   member this.replace cards =
     c <- cards
-  new()=Hand([||])
+  new()=
+    Hand([||])
   
-///
-///
-///
-type Player(name, index, AI) =
+/// <summary>Player is an object representing a Player (AI or human). A Player 
+/// has a name, index (representing order of game-flow), a hand cards, and a 
+/// score.
+/// The score is updated when called, and are to determine if a player has bust
+/// (over 21 points).</summary>
+/// <param name="name">String representing the name of the player</param>
+/// <param name="index">Integer represebting the index of the position in 
+/// Player Array in Game object</param>
+/// <param name="AI">Boolean representing whether a player is a NPC 
+/// (Non-Playable Character) or PC (Playable Character)</param>
+type Player(name,index,AI) =
   let h = new Hand()
   member this.name:string = name
   member this.index:int = index
-  member this.hand with get() = h
+  member this.hand = h
   member this.AI:bool = AI
   member this.score =
-    //implement 1 or 11 with A
     let mutable score = 0
     let mutable es = 0
     for card in this.hand.cards do
@@ -75,16 +82,18 @@ type Player(name, index, AI) =
       score <- score+10
       es <- es-1
     score
-  member this.isBusted()=(this.score>21)
-  new(name, index)=
+  member this.isBusted() = (this.score>21)
+  new(name, index) =
     Player(name, index, false)
 
-///
-///
-///
-type Game(dealer, players, numberOfPlayers) =
+/// <summary>Game is an object which is used to contain a collection of data,
+/// for which is used in-game, like players, a dealer, and a card stack.</summary>
+/// <param name="dealer">A Player object representing a dealer. Player.AI must be
+/// set to true.</param>
+/// <param name="players">An Array of Player objects.</param>
+type Game(dealer,players) =
   let s = new Hand()
   member this.dealer = dealer
   member this.players = players
-  member this.numberOfPlayers = numberOfPlayers
+  member this.numberOfPlayers = Array.length players
   member this.stack = s
