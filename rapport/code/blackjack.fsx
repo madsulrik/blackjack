bb5594a566d8a3efdc96e72a07d1294ed3600be8
Bo Hans Thomsen <bht@it-minds.dk>
2015-12-30 21:29:30 +0100

    UML files and some design text

---
+++ rapport/code/blackjack.fsx
@@ -0,0 +1,159 @@
+// Console helperfunctions
+let write (str:string) = System.Console.Write str
+let writeln (str:string) = System.Console.WriteLine str
+let readln() = System.Console.ReadLine()
+let setcursor(x,y) = System.Console.SetCursorPosition(x,y)
+let clear() = System.Console.Clear()
+
+/// <summary>Enumeration representing card-suits</summary>
+type Suits = Spades | Hearts | Diamonds | Clubs
+
+/// <summary>Card is an object representing af card, with value and suit</summary>
+/// <param name="value">Integer representing the card value. 1 is A, 11-13 are 
+/// picturecards</param>
+/// <param name="suit">Suits enumeration representing the suit of the card</param>
+type Card(value,suit) =
+  member this.value:int = value
+  member this.suit:Suits = suit
+  member this.toString() =
+    let suit =
+      match this.suit with
+      | Spades -> "spade"
+      | Hearts -> "heart"
+      | Diamonds -> "diamond"
+      | Clubs -> "club"
+    let value =
+      match this.value with
+      | 1 ->  "A"
+      | 11 -> "J"
+      | 12 -> "Q"
+      | 13 -> "K"
+      | x -> sprintf "%d" x
+    sprintf "%s%s" suit value
+
+/// <summary>Hand is an object representing a players hand. A Hand-object can 
+/// draw (Hand.draw card) or drop (Hand.drop) a card. The hand can be shuffled 
+/// (Hand.shuffle) and replaced by a new Card Array (Hand.replace cards) </summary>
+/// <param name="hand">Card Array that represents the cards on the hand (optional)</param>
+type Hand(hand) = 
+  let mutable c:(Card array) = hand
+  member this.cards with get() = c
+  member this.drop =
+    let lastIndex = (Array.length c)-1 
+    let card = c.[0]
+    c <- c.[1..lastIndex]
+    card    
+  member this.draw (card:Card) = c <- Array.append [|card|] c
+  member this.toString() =
+    let mutable str = ""
+    for i=0 to (Array.length c)-1 do
+      if i>0 then str <- str + " "
+      str <- str + c.[i].toString()
+    str
+  member this.shuffle() = 
+    let len = Array.length c
+    let testCard = Card(-1,Spades)
+    let newHand = Array.create len testCard
+    let test (card:Card) = card.toString()=testCard.toString()
+    let rnd = System.Random()
+    for i in 0..(len-1) do
+      let mutable j = rnd.Next(0,len)
+      while test c.[j] do
+        j <- rnd.Next(0,len)
+      newHand.[i] <- c.[j]
+      c.[j] <- testCard
+    c <- newHand
+  member this.replaceWith cards =
+    c <- cards
+  new()=
+    Hand([||])
+  
+/// <summary>Player is an object representing a Player (AI or human). A Player 
+/// has a name, index (representing order of game-flow), a hand cards, and a 
+/// score.
+/// The score is updated when called, and are to determine if a player has bust
+/// (over 21 points).</summary>
+/// <param name="name">String representing the name of the player</param>
+/// <param name="index">Integer represebting the index of the position in 
+/// Player Array in Game object</param>
+/// <param name="AI">Boolean representing whether a player is a NPC 
+/// (Non-Playable Character) or PC (Playable Character)</param>
+type Player(name,index,AI) =
+  let h = new Hand()
+  member this.name:string = name
+  member this.index:int = index
+  member this.hand = h
+  member this.AI:bool = AI
+  member this.score =
+    let mutable score = 0
+    let mutable es = 0
+    for card in this.hand.cards do
+      if card.value = 1 then es <- es + 1
+      if card.value > 10 then
+        score <- score + 10
+      else
+        score <- score + card.value
+    while es>0 && floor(float(21-score)/10.)>=1. do
+      score <- score+10
+      es <- es-1
+    score
+  member this.isBusted() = (this.score>21)
+  member this.scoreboard() =
+    let space = if (this.index+1)%3=0 && this.index <> 0 then "" else "  "
+    let top =    "" + space
+    let middle = "" + space
+    let empty =  "" + space
+    let bottom = "" + space
+    let x = (String.length top - String.length space)*(this.index%3) + (this.index)%3
+    let y = (8*(this.index/3)+5)
+    System.Console.SetCursorPosition(x,y)
+    System.Console.Write top
+    System.Console.SetCursorPosition(x,y+1)
+    System.Console.Write empty
+    let xn = (String.length empty)/2 - (String.length this.name)/2
+    System.Console.SetCursorPosition(x+xn,y+1)
+    System.Console.Write this.name
+    System.Console.SetCursorPosition(x,y+2)
+    System.Console.Write middle
+    System.Console.SetCursorPosition(x,y+3)
+    System.Console.Write empty
+    let xc = (String.length empty)/2 - (String.length (h.toString()))/2
+    System.Console.SetCursorPosition(x+xc,y+3)
+    System.Console.Write (h.toString())
+    System.Console.SetCursorPosition(x,y+4)
+    System.Console.Write empty
+    let score = sprintf "(%d)" this.score
+    let xs = (String.length empty)/2 - (String.length (score))/2
+    System.Console.SetCursorPosition(x+xs,y+4)
+    System.Console.Write score
+    System.Console.SetCursorPosition(x,y+5)
+    System.Console.Write middle
+    System.Console.SetCursorPosition(x,y+6)
+    System.Console.Write empty
+    System.Console.SetCursorPosition(x,y+7)
+    System.Console.Write bottom
+  new(name, index) =
+    Player(name, index, false)
+
+/// <summary>Game is an object which is used to contain a collection of data,
+/// for which is used in-game, like players, a dealer, and a card stack.
+/// The Game object is responsible for transfering cards from the stack to the 
+/// players.</summary>
+/// <param name="dealer">A Player object representing a dealer. Player. 
+/// AI must be set to true.</param>
+/// <param name="players">An Array of Player objects.</param>
+type Game(dealer,players) =
+  let s = new Hand()
+  do
+    let mutable cards = [||]:(Card array)
+    for i=1 to 13 do
+      cards <- Array.append cards [|Card(i,Hearts);Card(i,Spades);Card(i,Diamonds);Card(i,Clubs)|]
+    s.replaceWith cards
+    s.shuffle()
+  member this.dealer:Player = dealer
+  member this.players:(Player array) = players
+  member this.numberOfPlayers = Array.length players
+  member this.stack = s
+  member this.draw (player:Player) =
+    if Array.length this.stack.cards > 0 then
+      player.hand.draw this.stack.drop
---
+++ rapport/code/game.fsx
@@ -0,0 +1,160 @@
+#load "./blackjack.fsx"
+#load "./headers.fsx"
+open Blackjack
+open Headers
+
+///
+///
+///
+let validate_name str = String.length str > 0 && String.length str < 25
+let validate_yn str = (str = "y" || str = "n")
+
+///
+///
+///
+let printScoreboard (game:Game) =
+  clear()
+  write mainHeader
+  for player in game.players do
+    player.scoreboard()
+  game.dealer.scoreboard()
+  System.Console.WriteLine ""
+  
+  
+///
+///
+///
+let selectPlayer (player:Player) = 
+  let c = (System.Console.CursorLeft,System.Console.CursorTop)
+  let fill = "|XXXXXXXXXXXXXXXXXXXXXXX|"
+  let x = (String.length fill)*(player.index%3) + (player.index)%3
+  let y = (8*(player.index/3)+11)
+  System.Console.SetCursorPosition(x,y)
+  System.Console.Write fill
+  System.Console.SetCursorPosition c
+
+///
+///
+///
+let AI (game:Game) (player:Player) =
+  let mutable bestValue = 0
+  for player in game.players do
+    let score = player.score
+    if score<22 && score>bestValue then bestValue <- score
+  System.Threading.Thread.Sleep(500)
+  let mutable IDare = true
+  while IDare do 
+    let diff = max 0 (21 - player.score)
+    let es = Array.filter (fun (x:Card)->x.value=1) game.stack.cards |> Array.length
+    let p = Array.filter (fun (x:Card)->x.value<=diff) game.stack.cards |> Array.length
+    let pos x = if x < 0 then -x else x 
+    if p > 40 || es > 0 && p+10 > 20 || p > 25 && pos (bestValue-player.score) < 4 then
+      System.Threading.Thread.Sleep((52-p)*60)
+      game.draw player
+    else
+      IDare <- false
+    printScoreboard game
+    selectPlayer player
+    
+///
+///
+///
+let rec main (game:Game) =
+  for player in game.players do
+    printScoreboard game
+    selectPlayer player
+    if player.AI=true then 
+      AI game player
+    else
+      let mutable command = ""
+      while command <> "stand" && player.score < 21 do
+        command <- readln()
+        if command = "hit" then
+          game.draw player
+        printScoreboard game
+        selectPlayer player
+  AI game game.dealer
+  let mutable winners = [||]:(Player array)
+  for player in game.players do
+    if player.isBusted()=false && player.score > game.dealer.score then
+      if (player.score=21 && game.dealer.score=21 && Array.length player.hand.cards=2 
+      && Array.length player.hand.cards = Array.length game.dealer.hand.cards)=false then
+      
+        winners <- Array.append winners [|player|]
+  if Array.length winners = 0 && game.dealer.score <= 21 then
+    writeln "Dealer was too good!"
+  elif Array.length winners = 0 then
+    writeln "No winners!"
+  else
+    writeln "And the winner(s) is:"
+  for winner in winners do
+    writeln (sprintf " dot %s (%d)" winner.name winner.score)
+  write "New round (y/n)?"
+  let mutable input = readln()
+  while validate_yn input = false do
+    clear()
+    write header
+    write "New round (y/n)?"
+    input <- readln()
+  if input = "y" then
+    for player in game.players do player.hand.replaceWith [||]
+    game.dealer.hand.replaceWith [||]
+    main(Game(game.dealer,game.players))
+
+///
+///
+///
+let setup() =
+  clear()
+  write header
+  let rec nop() =
+    clear()
+    write header
+    write "Number of players (1-5): "
+    let c = 
+      try
+        readln() |> int
+      with
+      | _ -> 0
+    if c < 1 || c > 5 then nop() else int c
+  let numberOfPlayers = nop()
+  let mutable players:(Player array) = [||]
+  for i=0 to numberOfPlayers-1 do
+    let mutable name = ""
+    while validate_name name = false do
+      clear()
+      write header
+      write (sprintf "Player %d's name is: " (i+1))
+      name <- readln()
+      writeln ""
+    let mutable input = ""
+    while validate_yn input = false do
+      clear()
+      write header
+      write (sprintf "Is player %d a human (y/n): " (i+1))
+      input <- readln()
+      writeln ""
+    let AI = input = "n"
+    if AI then name <- name + "(AI)"
+    players <- Array.append players [|Player(name,i,AI)|]
+  clear()
+  let dealer = Player("Dealer",numberOfPlayers,true)
+  main(Game(dealer,players))
+
+///
+///
+///
+let rec menu() =
+  clear()
+  write menuHeader
+  let input = System.Console.ReadKey()
+  System.Threading.Thread.Sleep(50)
+  match input.KeyChar with
+  | '1' -> 
+    setup()
+    menu()
+  | '2' -> 
+    clear()
+    exit 0
+  | _ -> menu()
+menu()
